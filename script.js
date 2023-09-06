894bb6fc27b98dd258bd8882fe3c2f765fc67611
Joseph Agramonte <lifeinabundance314@gmail.com>
2023-09-06 12:55:23 -0400

    add style.css and script.js to html

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This is created by JavaScript";
+h2.textContent = "Memories With My Family in Paris";
 document.querySelector("body").appendChild(h2);
+
+    const originalWidth = image.width;
+    const originalHeight = image.height;
+  
